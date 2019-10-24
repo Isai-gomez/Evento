53f9248deb1ee655edcbadc27f95d767cd21b9d0
Chai <isaigomezhernandez.com>
2019-10-24 14:30:17 -0500

    añadiendo las opciones a la seccion

--- src/component/Formulario.js
+++ src/component/Formulario.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 class Formulario extends Component {
+  mostrarOpciones = key => {
+    const categori = this.props.categorias[key];
+    const { id, name_localized } = categori;
+    if (!id || !name_localized) return null;
+    return (
+      <option key={id} value={id}>
+        {name_localized}
+      </option>
+    );
+  };
   render() {
+    const categoria = Object.keys(this.props.categorias);
+    console.log(categoria);
     return (
       <form>
         <fieldset className="uk-fieldset uk-margin">
@@ -16,7 +28,9 @@ class Formulario extends Component {
               />
             </div>
             <div className="uk-margin" uk-margin="true">
-              <select className="uk-select"></select>
+              <select className="uk-select">
+                {categoria.map(this.mostrarOpciones)}
+              </select>
             </div>
             <div className="uk-margin" uk-margin="true">
               <button className="uk-input uk-button-danger">Buscar</button>
