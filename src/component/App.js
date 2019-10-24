fd1e128821037cd60436778315f48832621b248d
Chai <isaigomezhernandez.com>
2019-10-24 11:42:20 -0500

    Obteniendo las categorias

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 import Header from "./Header";
 class App extends Component {
+  componentDidMount() {
+    this.obtenerCategoria();
+  }
+
+  obtenerCategoria = async () => {
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=DQMN3QQLB6WN2YG7ZTE6&locale=es_ES`;
+    await fetch(url)
+      .then(respuesta => {
+        return respuesta.json();
+      })
+      .then(categoria => {
+        console.log(categoria.categories);
+      });
+  };
   render() {
     return (
       <div className="App">
